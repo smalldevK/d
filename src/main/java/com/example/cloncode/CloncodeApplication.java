72e1c4080b9136bdeb46bda33024fb37d33095be
Kim <wwwmnt@naver.com>
2023-10-27 21:52:14 +0900

    first commit

---
+++ src/main/java/com/example/cloncode/CloncodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloncodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloncodeApplication.class, args);
+	}
+
+}
